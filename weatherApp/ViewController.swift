702b65b8d85bfb67351afc03ac3eb8bf7fd2a982
PageKallop <PageMarie78@gmail.com>
2020-12-09 13:36:14 -0600

    First commit Created the UI Set up UITextField

--- weatherApp/ViewController.swift
+++ weatherApp/ViewController.swift
@@ -7,13 +7,52 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+   
+    @IBOutlet weak var conditionImageView: UIImageView!
+    
+    @IBOutlet weak var tempLabel: UILabel!
+    
+    @IBOutlet weak var cityLabel: UILabel!
+    
+    @IBOutlet weak var searchTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        searchTextField.delegate = self
     }
 
+    
+    @IBAction func searchPressed(_ sender: UIButton) {
+        
+        searchTextField.endEditing(true)
+    }
+    
+    // returns textfield input
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        searchTextField.endEditing(true)
+        return true
+    }
+    
+    // empty textfield
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        
+        searchTextField.text = ""
+    }
+    
+    // validates user input 
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        
+        if searchTextField.text != "" {
+            return true
+        } else {
+            searchTextField.placeholder = "Type a city"
+            return false
+        }
+    }
+    
 
 }
 
